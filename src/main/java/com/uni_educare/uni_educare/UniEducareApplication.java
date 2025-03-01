08d06e5cfbcc2654bee1783a9add520e28ddfd82
Arghya Dey <arghyadey639@gmail.com>
2025-03-01 13:01:12 +0530

    Uni-educare project initiated

---
+++ src/main/java/com/uni_educare/uni_educare/UniEducareApplication.java
@@ -0,0 +1,13 @@
+package com.uni_educare.uni_educare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniEducareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniEducareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni_educare/uni_educare/UniEducareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni_educare.uni_educare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniEducareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
