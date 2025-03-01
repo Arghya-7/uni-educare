389ffabe3ccd40ca8189d65b0cb8f8cced92ec36
Arghya Dey <arghyadey639@gmail.com>
2025-03-01 13:12:40 +0530

    added logger in the project

--- src/main/java/com/uni_educare/uni_educare/UniEducareApplication.java
+++ src/main/java/com/uni_educare/uni_educare/UniEducareApplication.java
@@ -1,13 +1,16 @@
 package com.uni_educare.uni_educare;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UniEducareApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(UniEducareApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(UniEducareApplication.class, args);
+		LOGGER.info("Project started");
 	}
 
 }
